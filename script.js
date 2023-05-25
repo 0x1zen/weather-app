aa8f1e4d8e47fe292a31f0247cd9d6ab768b4dfd
0x1zen <rajdubalwork@gmail.com>
2023-05-25 12:43:53 +0530

    called classes and custom attributes to js using querySelector

---
+++ script.js
@@ -0,0 +1,7 @@
+const userTab=document.querySelector("[data-userWeather]");
+const searchTab=document.querySelector("[data-searchWeather]");
+const userContainer=document.querySelector(".weather-container");
+const grantAccessContainer=document.querySelector(".grant-location-container");
+const searchForm=document.querySelector("[data-searchForm]");
+const leadingScreen=document.querySelector(".loading-container");
+const userInfoContainer=document.querySelector(".user-info-container")
