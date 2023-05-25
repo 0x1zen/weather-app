cb0bb607be81ad6ffcfd050b57cb32c750c91eb3
0x1zen <rajdubalwork@gmail.com>
2023-05-25 13:31:39 +0530

    added eventListener to userTab

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ const userInfoContainer=document.querySelector(".user-info-container");
 let currentTab=userTab;
 let API_KEY="3ff3b550f32b88b5b9814fa296242404";
 currentTab.classList.add("current-tab");
+
+userTab.addEventListener("click",()=>{
+    // pass clicked tab as input parameter
+switchTab(userTab)
+});
+searchTab.addEventListener("click",()=>{
+    // pass clicked tab as input parameter
+switchTab(searchTab)
+});
