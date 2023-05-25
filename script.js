e5ef53149f80b9de1c5030202fa72751a7244cc7
0x1zen <rajdubalwork@gmail.com>
2023-05-25 13:20:15 +0530

    initialized few variables and added a classList for current-tab

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ const userContainer=document.querySelector(".weather-container");
 const grantAccessContainer=document.querySelector(".grant-location-container");
 const searchForm=document.querySelector("[data-searchForm]");
 const leadingScreen=document.querySelector(".loading-container");
-const userInfoContainer=document.querySelector(".user-info-container")
+const userInfoContainer=document.querySelector(".user-info-container");
+
+// initial variables
+
+let currentTab=userTab;
+let API_KEY="3ff3b550f32b88b5b9814fa296242404";
+currentTab.classList.add("current-tab");
