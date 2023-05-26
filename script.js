8fdb69ada280fb6129a92668ee048130b74b7866
0x1zen <rajdubalwork@gmail.com>
2023-05-26 11:10:11 +0530

    initially declared function getFromSessionStorage to get coords

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let currentTab=userTab;
 // let API_KEY="3ff3b550f32b88b5b9814fa296242404";
 let API_KEY="d1845658f92b31c64bd94f06f7188c9c";
 currentTab.classList.add("current-tab");
+getFromSessionStorage();
 
 
 function switchTab(clickedTab){
